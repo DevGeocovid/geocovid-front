337720742e017cdecfe0ca2861f3b2df04fe2584
WillCQ97 <willian.cqueiroz@gmail.com>
2020-11-17 14:32:39 -0300

    Alterações de informações da página.

--- pages/generated.vue
+++ pages/generated.vue
@@ -4,21 +4,20 @@
     :style="$vuetify.breakpoint.xs ? 'padding-top:120px' : ''"
   >
     <span class="display-3 font-weight-medium color-blue">
-      Estes são dados <br />
-      gerados pelo projeto
+      Dados gerados pelo projeto
     </span>
     <br />
     <br />
+    
+    <!-- FIX-ME: TEXTO TEMPORÁRIO, APENAS PARA REMOVER O LOREM IPSUM DO SITE PRINCIPAL -->
+    <!-- FIX-ME: A FONTE DO TEXTO ESTÁ BEM PEQUENA-->
+    <!-- FIX-ME: ADICIONAR CARD PARA OS MAPAS DE VULNERABILIDADE -->
     <p class="body-2 text-justify">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. A dolore impedit
-      mollitia, itaque sapiente facilis ut, odio ducimus, veniam provident
-      deserunt alias aspernatur. Suscipit, et laborum aut provident saepe vero.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ipsam sunt
-      sed vero eum beatae eos ad assumenda, sapiente, praesentium delectus
-      inventore aperiam perspiciatis odit quaerat enim et adipisci voluptate!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ipsam sunt
-      sed vero eum beatae eos ad assumenda, sapiente, praesentium delectus
-      inventore aperiam perspiciatis odit quaerat enim et adipisci voluptate!
+      A seguir são apresentados gráficos e mapas gerados pelo projeto disponíveis para visualização e download.
+    </p>
+    <p class="body-2 text-justify">
+      Na visualização para os mapas, encontran-se as pastas relativas a cada mês, e nelas estão contidas os mapas 
+      de novos casos diagnosticados e de casos acumulados para cada dia do respectivo mês.
     </p>
     <br />
     <br />
@@ -37,10 +36,10 @@
      <br />
     <span
       class="title font-weight-medium color-blue subtitle-main d-flex justify-center mt-12"
-      >MAPAS DE CASOS ACOMULADOS</span
+      >MAPAS DE CASOS DISPONÍVEIS PARA DOWNLOAD</span
     >
     <v-card class="mt-4" width="100%" height="800px">
-      <PictureMapView />
+      <CardMapsFiles />
     </v-card>
   </div>
 </template>
