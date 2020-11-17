4e7511ee7afd38fcca267a36d9badae7a2b74054
WillCQ97 <willian.cqueiroz@gmail.com>
2020-11-17 13:31:33 -0300

    Divisão do texto sobre o projeto em parágrafos

--- pages/about.vue
+++ pages/about.vue
@@ -4,39 +4,56 @@
     :style="$vuetify.breakpoint.xs ? 'padding-top:120px' : ''"
   >
     <span class="display-3 font-weight-medium color-blue">
-      Entenda o projeto Geocovid
+      Entenda o projeto
     </span>
     <br />
     <br />
-
-    <p class="body-2 text-justify">
-      De acordo com o Ministério da Saúde (2020) o Brasil confirmou em 13 de
-      maio de 2020, 188.974 casos de COVID-19 e 13.149 óbitos, possui taxa de
-      letalidade de 7%, ocupando a nona posição no ranking mundial da
-      Organização Mundial da Saúde. No Espírito Santo, na mesma data,
-      confirmaram-se 5.401 casos, com 233 óbitos, com taxa de letalidade de
-      aproximadamente 4,3% (PAINELCOVID19ES, 2020). O Governo do Espírito Santo
-      adotou um Mapa de Gestão de Risco para estabelecer medidas direcionadas
-      para cada município, porém leva-se em consideração somente algumas
-      estatísticas, classificando os municípios em Baixo, Moderado e Alto risco.
-      Diferente do que vem sendo feito, esse projeto propõe o estudo do
-      comportamento da dispersão da doença, o vetor de crescimento, a densidade
-      de casos confirmados e associando a isso a vulnerabilidade social de
-      alguns municípios selecionados, podendo revelar uma tendência temporal da
-      prevalência de contaminação. O monitoramento da evolução espacial do
-      Covid-19 é de suma importância para auxiliar na tomada de decisões quanto
-      às ações para contenção da contaminação, planejamento da gestão de
-      recursos e na movimentação de profissionais para áreas com maior
-      vulnerabilidade. Assim, através do banco de dados disponibilizado pelo
-      Painel COVID-19-Espírito Santo serão elaborados mapas atualizados
-      periodicamente, que serão úteis para indicar a tendência de contaminação
-      no estado, munindo os gestores de ferramenta para tomada de decisões em
-      alvos potenciais e preferenciais de políticas públicas. Como não podemos
-      recolher amostras na população inteira, estes mapas podem ser úteis para
-      indicar locais a serem amostrados de forma mais racional e decidir que
-      regiões estão mais ou menos preparadas para sair do isolamento de forma
-      estratificada.
-    </p>
+    <ul>
+      <li>
+        <p class="body-2 text-justify">
+          De acordo com o Ministério da Saúde (2020) o Brasil confirmou em 13 de
+          maio de 2020, 188.974 casos de COVID-19 e 13.149 óbitos, possui taxa
+          de letalidade de 7%, ocupando a nona posição no ranking mundial da
+          Organização Mundial da Saúde. No Espírito Santo, na mesma data,
+          confirmaram-se 5.401 casos, com 233 óbitos, com taxa de letalidade de
+          aproximadamente 4,3% (PAINELCOVID19ES, 2020). O Governo do Espírito
+          Santo adotou um Mapa de Gestão de Risco para estabelecer medidas
+          direcionadas para cada município, porém leva-se em consideração
+          somente algumas estatísticas, classificando os municípios em Baixo,
+          Moderado e Alto risco.
+        </p>
+      </li>
+      <li>
+        <p class="body-2 text-justify">
+          Diferente do que vem sendo feito, esse projeto propõe o estudo do
+          comportamento da dispersão da doença, o vetor de crescimento, a
+          densidade de casos confirmados e associando a isso a vulnerabilidade
+          social de alguns municípios selecionados, podendo revelar uma
+          tendência temporal da prevalência de contaminação.
+        </p>
+      </li>
+      <li>
+        <p class="body-2 text-justify">
+          O monitoramento da evolução espacial do Covid-19 é de suma importância
+          para auxiliar na tomada de decisões quanto às ações para contenção da
+          contaminação, planejamento da gestão de recursos e na movimentação de
+          profissionais para áreas com maior vulnerabilidade.
+        </p>
+      </li>
+      <li>
+        <p class="body-2 text-justify">
+          Assim, através do banco de dados disponibilizado pelo Painel
+          COVID-19-Espírito Santo serão elaborados mapas atualizados
+          periodicamente, que serão úteis para indicar a tendência de
+          contaminação no estado, munindo os gestores de ferramenta para tomada
+          de decisões em alvos potenciais e preferenciais de políticas públicas.
+          Como não podemos recolher amostras na população inteira, estes mapas
+          podem ser úteis para indicar locais a serem amostrados de forma mais
+          racional e decidir que regiões estão mais ou menos preparadas para
+          sair do isolamento de forma estratificada.
+        </p>
+      </li>
+    </ul>
 
     <br />
     <br />
@@ -52,18 +69,18 @@
           <a
             href="https://coronavirus.es.gov.br/painel-covid-19-es"
             target="_blank"
-            style="color: #666666;"
+            style="color: #666666"
             >https://coronavirus.es.gov.br/painel-covid-19-es</a
           >. Acesso em: 20 out. 2020.
         </p>
       </li>
       <li>
         <p>
-          GEOBASES. <b>Portal do sistema GEOBASES </b>. Disponível em:
+          GEOBASES. <b>Portal do sistema GEOBASES</b>. Disponível em:
           <a
             href="https://geobases.es.gov.br"
             target="_blank"
-            style="color: #666666;"
+            style="color: #666666"
           >
             https://geobases.es.gov.br</a
           >. Acesso em: 20 out. 2020.
@@ -77,7 +94,7 @@
           <a
             href="https://censo2010.ibge.gov.br/"
             target="_blank"
-            style="color: #666666;"
+            style="color: #666666"
           >
             https://censo2010.ibge.gov.br/</a
           >. Acesso em: 20 out. 2020.
@@ -93,7 +110,7 @@
           <a
             href="http://ivs.ipea.gov.br/images/publicacoes/Ivs/publicacao_atlas_ivs_rm.pdf"
             target="_blank"
-            style="color: #666666;"
+            style="color: #666666"
           >
             http://ivs.ipea.gov.br/images/publicacoes</a
           >. Acesso em: 20 out. 2020.
@@ -110,7 +127,7 @@
           <a
             href="http://repositorio.ufes.br/handle/10/1168"
             target="_blank"
-            style="color: #666666;"
+            style="color: #666666"
           >
             http://repositorio.ufes.br/handle/10/1168</a
           >. Acesso em: 20 out. 2020.
@@ -268,24 +285,24 @@
       class="d-flex justify-center align-center mt-4"
       :class="$vuetify.breakpoint.xs ? 'flex-column' : ''"
     >
-      <img src="~/assets/logos/ufes.png" alt="ufes" style="width: 150px;" />
+      <img src="~/assets/logos/ufes.png" alt="ufes" style="width: 150px" />
       <img
         src="~/assets/logos/ccens.png"
         alt="ccens"
         class="ml-4"
-        style="width: 200px;"
+        style="width: 200px"
       />
       <img
         src="~/assets/logos/fapes.png"
         alt="fapes"
         class="ml-4"
-        style="width: 200px;"
+        style="width: 200px"
       />
       <img
         src="~/assets/logos/es.png"
         alt="es"
         class="ml-4"
-        style="width: 200px;"
+        style="width: 200px"
       />
     </div>
   </div>
