d2a6470cc1a93c6bd9b341b0bd652a6e47fa353f
matheus <matheusdasilvarocha@gmail.com>
2020-12-01 21:36:01 -0300

    Inclusao do botao para download do shape file

--- pages/generated.vue
+++ pages/generated.vue
@@ -11,10 +11,10 @@
     <br />
     <p class="body-2 text-justify">
       A seguir são apresentados gráficos e mapas gerados pelo projeto
-      disponíveis para visualização e download.
-      Na visualização para os mapas, encontram-se as pastas relativas a cada
-      mês, e nelas estão contidos os mapas de novos casos diagnosticados e de
-      casos acumulados para cada dia do respectivo mês.
+      disponíveis para visualização e download. Na visualização para os mapas,
+      encontram-se as pastas relativas a cada mês, e nelas estão contidos os
+      mapas de novos casos diagnosticados e de casos acumulados para cada dia do
+      respectivo mês.
     </p>
     <br />
     <br />
@@ -30,14 +30,29 @@
     >
       <CasesTimeLapse />
     </v-card>
-     <br />
+    <br />
+
     <span
       class="title font-weight-medium color-blue subtitle-main d-flex justify-center mt-12"
-      >MAPAS DE CASOS ACOMULADOS</span
+      >ARQUIVOS DOS MAPAS DE CASOS ACUMULADOS</span
     >
     <v-card class="mt-4" width="100%" height="800px">
       <PictureMapView />
     </v-card>
+    <br />
+    <div class="mt-4 d-flex justify-center align-center">
+      <span>Link p/ download shape-file</span>
+
+      <a
+        style="text-decoration: none;"
+        target="_blank"
+        href="https://drive.google.com/drive/folders/1szOZkvBJclkUhIhj1XCAo5D5X6NqPKC6?usp=sharing"
+      >
+        <v-btn small class="ml-2" color="primary" rounded elevation="0">
+          Download <v-icon dense>mdi-arrow-down</v-icon></v-btn
+        >
+      </a>
+    </div>
   </div>
 </template>
 
