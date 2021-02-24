bd176bb897d519f81b55ce16b6bc7a4a4158e372
gabrielnama2 <46804141+gabrielnama2@users.noreply.github.com>
2021-02-24 17:38:53 -0300

    Atualização do carrossel

--- components/Carrossel/index.vue
+++ components/Carrossel/index.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="container-fluid">
-    <v-carousel cycle class="carrossel">
+    <v-carousel cycle="2s" class="carrossel">
       <a target="blank" style="text-decoration: none;" href="/news">
         <v-carousel-item
           target="_blank"
-          v-for="(item,i) in items"
+          v-for="(slide, i) in slides"
           :key="i"
-          :src="item.src"
           reverse-transition="fade-transition"
           transition="fade-transition"
         >
+        <v-img
+          height="100%"
+          src="https://bn1304files.storage.live.com/y4msLbTAx_Kl9KtXDT2EkixqNbUtlWY1veU5Vy773q080HeS6ZZ3HVKro19IFXPbNhk2jTkcYAE9I1JVx78AnQWKCVLRG09dbBXZkozJV3YE98w4fDlyCDlQZMNcpZ-9GsUOh_YCVfOanDPf0dcx5VsWjvWQGtJkFpawof_SGN7lYfm6jEbydDMCb5RC9Yp4U_O?width=1300&height=600&cropmode=none"
+        >
         <!--Legenda-->
         <div class="display-3">
-          <h1 class="display">Veja as principais notícias <br>sobre covid-19 no Espírito Santo</h1>
+          <h1 class="display">{{ slide }}</h1>
         </div>
+        </v-img>
         </v-carousel-item>
       </a>
     </v-carousel>
@@ -29,6 +33,13 @@ export default {
           src: 'https://bn1304files.storage.live.com/y4msLbTAx_Kl9KtXDT2EkixqNbUtlWY1veU5Vy773q080HeS6ZZ3HVKro19IFXPbNhk2jTkcYAE9I1JVx78AnQWKCVLRG09dbBXZkozJV3YE98w4fDlyCDlQZMNcpZ-9GsUOh_YCVfOanDPf0dcx5VsWjvWQGtJkFpawof_SGN7lYfm6jEbydDMCb5RC9Yp4U_O?width=1300&height=600&cropmode=none',
         },
       ],
+      slides: [
+          'Máscaras com válvulas foram proibidas nos voos no ES',
+          'ES agenda vacinação para profissionais fora da linha de frente',
+          'Covid-19: nove erros de higienização que as pessoas ainda cometem',
+          'Dois pacientes de Manaus curados de Covid-19 no ES',
+          'Retrospestiva de 2020 dos casos de covid-19 no Espírito Santo',
+        ],
     }
   },
 }
@@ -45,8 +56,8 @@ export default {
   font-weight: 500;
   opacity: 1;
   text-align: center;
-  margin: 0;
-  line-height: 70px;
+  margin: 200px;
+  line-height: 60px !important;
   transition: background-color .3s ease-in-out;
   color: white;
   padding: 0;
@@ -59,9 +70,10 @@ export default {
   }
   .display{
     font-size: 35px !important;
-    line-height: 50px;
+    line-height: 45px !important;
     padding: 50px;
-    margin-top: 0px;
+    margin-top: 25px !important;
+    margin: 0;
   }
 }
 /**MOBILE LANDSCAPE**/
@@ -72,7 +84,7 @@ export default {
   .display{
     font-size: 38px !important;
     padding: 60px;
-    margin-top: 0px;
+    margin-top: 0px !important;
   }
 }
 @media (min-width: 720px) and (min-height: 800px){
@@ -83,7 +95,8 @@ export default {
     font-size: 30px !important;
     font-weight: 500;
     line-height: 50px;
-    margin-top: 30px;
+    margin-top: 35px !important;
+    margin: 20px;
   }
 }
 /**PC 720p**/
@@ -91,7 +104,7 @@ export default {
   .display{
     font-size: 42px !important;
     padding: 50px;
-    margin-top: 50px;
+    margin-top: 40px !important;
   }
 }
 /**PC 1080p**/
@@ -102,7 +115,9 @@ export default {
   .display{
     font-size: 50px !important;
     padding: 0;
-    margin-top: 130px;
+    margin-top: 130px !important;
+    margin: 200px ;
+    line-height: 70px !important;
   }
 }
 </style>
