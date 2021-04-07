f63145a091cff0e9913922ff166a01427146c155
gabrielnama2 <46804141+gabrielnama2@users.noreply.github.com>
2021-04-07 17:20:15 -0300

    Nova notícia adicionada ao site

--- components/Carrossel/index.vue
+++ components/Carrossel/index.vue
@@ -35,6 +35,7 @@ export default {
       ],
       slides: [
         'Timelapse: Casos de Covid-19 no Espírito Santo em março',
+        'Brasil lidera mortes diárias por Covid-19 no mundo em março',
         'Uma morte a cada 15 minutos: ES bate recorde de mortes em 24 horas',
         'ES bate recorde e ultrapassa marca de 900 pessoas em leitos de UTI',
         'Março foi o mês com mais mortes desde o início da pandemia no ES',
