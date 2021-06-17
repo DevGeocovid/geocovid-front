8ac18530485b24c6d149d5187807ea0e43d8c231
gabrielnama2 <46804141+gabrielnama2@users.noreply.github.com>
2021-06-17 18:33:32 -0300

    (Carrossel) Novas notícias

--- components/Carousel/index.vue
+++ components/Carousel/index.vue
@@ -30,10 +30,9 @@ export default {
 
       titles: [
         'Timelapse: Casos de mortes pela Covid-19 no ES em 1 ano',
-        'Covid-19 no ES já custou R$ 1 bilhão; mais da metade foi gasta em leitos',
+        'ES começa a testar passageiros para Covid-19 no Aeroporto de Vitória',
+        'Auditoria diz que 934 pessoas usaram documentos de mortos na vacinação contra Covid-19 no ES',
         'Vacinação em massa: quando o ES sentirá os efeitos da imunidade coletiva?',
-        'ES é o 7° em ranking com mais mortes por 100 mil habitantes',
-        '18 cidades do ES ainda guardam vacinas em geladeiras domésticas',
         'Brasil lidera mortes diárias por Covid-19 no mundo em março',
         'Retrospestiva de 2020 dos casos de Covid-19 no ES',
       ],
